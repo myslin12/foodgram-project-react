b81a8bd8bc33fac4dec32f6e6cffc0638525e5bb
myslin12 <128624345+myslin12@users.noreply.github.com>
2023-12-14 01:42:08 +0300

    review 2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -105,13 +105,6 @@ DATABASES = {
 
 FIXTURE_DIRS = '../../data'
 
-# Для работы с pq
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
