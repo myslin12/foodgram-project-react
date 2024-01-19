36a49f076b9f5470934d5741d01f3e0a59b3388a
Мыслин Владимир <vm.myslin@gmail.com>
2024-01-19 15:00:16 +0300

    review 4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -81,14 +81,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
-   'default': 
-   {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-       'USER': os.getenv('POSTGRES_USER', 'postgres'),
-       'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-       'HOST': os.getenv('DB_HOST', 'db'),
-       'PORT': os.getenv('DB_PORT', 5432)
+   'default':
+   {'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+    'USER': os.getenv('POSTGRES_USER', 'postgres'),
+    'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+    'HOST': os.getenv('DB_HOST', 'db'),
+    'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
