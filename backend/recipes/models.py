2b4f0e774ef4776d84b570796ab68860fc3b64e3
Мыслин Владимир <vm.myslin@gmail.com>
2024-03-03 22:55:08 +0300

    final 2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,6 +2,7 @@ from colorfield.fields import ColorField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
+
 from users.models import User
 
 
